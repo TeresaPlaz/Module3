6d8955a2c59dfa4c97a26be47d1d0978446a3331
Teresa Plaz <teresaplaz@gmail.com>
2018-08-04 20:45:22 -0400

    3rd iteration

--- lab-typescript/starter-code/iteration-2/main.ts
+++ lab-typescript/starter-code/iteration-2/main.ts
@@ -4,14 +4,40 @@
 // Execution
 import { todoInterface } from "./todoInterface";
 
+let myArray1 = [];
+
 class Todo implements todoInterface {
-  
-  constructor() {
-    
+
+  constructor(public myArray2: Array<string>) {}
+
+  addTask(target: string): number {
+    this.myArray2.push(target);
+    console.log(`
+    =========== NEW TASK =========== 
+    Task "${target}" inserted in the list`);
+
+    console.log(`Number of items: ${this.myArray2.length}`);
+    return this.myArray2.length;
   }
+
+  listAllTasks(): void {
+    console.log(...this.myArray2);
+  }
+
+  deleteTask(target:string): number {
+    let tempIndex: number = this.myArray2.indexOf(target);
+    this.myArray2.splice(tempIndex,1);
+    console.log(`
+    =========== TASK REMOVED ===========
+    Task "${target}" removed from the list`);
+
+    console.log(`Number of items: ${this.myArray2.length}`);
+    return this.myArray2.length;
+  } 
+
 }
 
-let myTodos = new Todo();
+let myTodos = new Todo(myArray1);
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
