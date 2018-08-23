1227c90e4d7ea11bace7c797b89a75a4d6631055
Teresa Plaz <teresaplaz@gmail.com>
2018-08-22 23:14:34 -0400

    update

--- lab-angular-cinema-billboard/starter-code/src/app/app.module.ts
+++ lab-angular-cinema-billboard/starter-code/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
 import { MyMovieComponentComponent } from './my-movie-component/my-movie-component.component';
+
+const routes: Routes = [
+  { path: ""  , redirectTo: "home", pathMatch: 'full'},
+  { path: "home", component: MyHomeComponentComponent},
+  { path: "movie/:id", component: MyMovieComponentComponent}
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +22,8 @@ import { MyMovieComponentComponent } from './my-movie-component/my-movie-compone
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
